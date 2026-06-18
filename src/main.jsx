fa6cf9c959fc046a6726b64cde39128e81ed210a
Emmanuel JR <emmanuelolorunbepelumi@gmail.com>
2026-06-18 10:54:26 -0700

    FIX

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,7 +6,7 @@ import { Provider } from 'react-redux'
 import { store } from './lib/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/gocart'>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
